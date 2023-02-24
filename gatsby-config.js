8d6468b7f088af34c8ad8c28d35239ed1d4f0ab3
Arden Choi <ardenyschoi@gmail.com>
2023-02-24 00:02:15 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Arden Choi`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
