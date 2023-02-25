5ecf9276b6f308bb4f410df68057eb355aa84eb8
Arden Choi <ardenyschoi@gmail.com>
2023-02-25 00:43:37 -0800

    contact form

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import * as React from "react"
 import About from "./about"
 import Contact from "./contact"
-import Contactcopy from "./contactcopy"
 import Footer from "./footer"
 import Intro from "./intro"
 import Menubar from "./menubar"
@@ -11,26 +10,14 @@ import Resume from "./resume"
 
 const IndexPage = () => {
   return (
-    // <main style={pageStyles}>
     <main>
       <Menubar />
       <Intro />
       <About />
       <Resume />
       <Portfolio />
-      <Contactcopy />
-      {/* <Contact /> */}
+      <Contact />
       <Footer />
-      {/* <Layout /> */}
-      {/* <!-- Java Script================================================== -->  */}
-      {/* <script src="js/jquery-2.1.3.min.js"></script>
-      <script src="js/plugins.js"></script>
-      <script src="js/main.js"></script> */}
-      {/* <Helmet>
-        <script src={withPrefix('../../static/js/main.js')} />
-        <script src={withPrefix('../../static/js/plugins.js')}  />
-        <script src={withPrefix('../../static/js/jquery-2.1.3.min.js')}  />
-      </Helmet> */}
    </main>
   )
 }
